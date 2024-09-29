e1824ab463324b70a10d649473d60b5239ff4818
Terry Gaul <trg01@trgaul.com>
2024-09-29 06:54:15 -0500

    Initial Commit

---
+++ Test_2024.09.29_A/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test_2024.09.29_A
+//
+//  Created by Terry Gaul on 9/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test_2024.09.29_A/Test_2024_09_29_AApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_2024_09_29_AApp.swift
+//  Test_2024.09.29_A
+//
+//  Created by Terry Gaul on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_2024_09_29_AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
